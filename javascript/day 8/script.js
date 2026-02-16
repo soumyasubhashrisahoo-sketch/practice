a0f36a8ea192896a3bee22369394eacbdf03183a
soumyasubhashrisahoo-sketch <soumyasubhashrisahoo@gmail.com>
2026-02-16 14:33:36 +0530

    Add files via upload

---
+++ javascript/day 8/script.js
@@ -0,0 +1,16 @@
+function handleClick() {
+    document.getElementById("heading")
+    .style.color = "red";
+}
+
+
+
+create addText()=>{
+
+    const newH2 = document.createElement("h2").textContent="
+    this is a sub heading"
+
+    const image = document.createElement("img");
+    ImageCapture.setAttribute = 'https://i.pinimg.com/736x/81/e7/75/81e7754d22e0094165e78e4f091eb404.jpg';
+    document.getElementbyId
+}
